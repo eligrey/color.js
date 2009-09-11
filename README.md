8a58c050828098a982b5f36eb4b7d4eeb818a6b3
Elijah Grey <elijah@grey.name>
2009-09-11 18:49:59 -0400

    v0.2 - see README.md

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 color.js
 ========
 
-*Version 0.1*
+*Version 0.2*
 
 Create and manipulate [RGBA](http://en.wikipedia.org/wiki/RGBA_color_space) colors with ease.
 
@@ -10,17 +10,21 @@ Examples
 
 ### Making violet (#EE82EE):
 
-    Color(0xEE82EE)         == "#EE82EE"
-    Color("#EE82EE")        == "#EE82EE"
-    Color("EE82EE")         == "#EE82EE"
-    Color(0xEE, 0x82, 0xEE) == "#EE82EE"
-    Color("EE", "82", "EE") == "#EE82EE"
-    Color(238, 130, 238)    == "#EE82EE"
+    Color.get("violet")
+    Color(0xEE82EE)
+    Color("#EE82EE")
+    Color("EE82EE")
+    Color(0xEE, 0x82, 0xEE)
+    Color("EE", "82", "EE")
+    Color(238, 130, 238)
+    Color.rgb(238, 130, 238)
+    Color.hsl(0.8333333333333334, 0.7605633802816902, 0.7215686274509804)
+    // 300°, 75.88652482269505%, 72.35294117647058%
 
 ### Shorthand color notation:
 
-    Color("#ABC") == "#AABBCC"
-    Color("ABC")  == "#AABBCC"
+    Color("#ABC") // "#AABBCC"
+    Color("ABC")  // "#AABBCC"
 
 ### Convert a hex color to RGBA:
 
@@ -31,72 +35,111 @@ Examples
 
     var myColor = new Color;
     myColor.setValue(0xAA7F00);
-    myColor == "#AA7F00"
-    myColor.channels.red = 0xFF;
-    myColor == "#FF7F00" // orange
+    myColor.hexTriplet() === "#aa7f00";
+    myColor.channels[0] = 0xFF;
+    myColor.hexTriplet() === "#ff7f00"; // orange
 
 ### Defining a custom color:
 
     Color.define("rind", [92, 163, 16])
-    Color.get("rind") == "#5CA310"
+    Color.get("rind").hexTriplet() === "#5ca310"
 
+### Get a random color between green and blue
+
+    Color.random("green", "blue");
 
 Supported Browsers
 ------------------
 
  * Firefox 1+
  * Safari 2+
- * IE 6+ (Color.get can't compute CSS color values)
+ * IE 6+ (`Color.get` can't compute CSS color values)
  * Opera 8+
  * Google Chrome 0.1+
 
 API
 ---
 
+**Note:** HSL values are in the form of fractions (0 to 1). A hue value of 0.5 is equivalent to a hue of 180&deg;.
+
 ### Instantiation
 
-color.js is flexible enough to allow you to instantiate color objects however you please:
+Color objects can be instantiated in any of the following ways:
 
 <pre><code>color = [new ]Color(<strong>color</strong>:int | string, [, <strong>alpha</strong>:float])
-color = [new ]Color(<strong>red</strong>:int | string, <strong>green</strong>:int | string, <strong>blue</strong>:int | string [, <strong>alpha</strong>:float])</code></pre>
+color = [new ]Color(<strong>red</strong>:int | string, <strong>green</strong>:int | string, <strong>blue</strong>:int | string [, <strong>alpha</strong>:float | string])
+color = Color.rgb(<strong>red</strong>:int, <strong>green</strong>:int, <strong>blue</strong>:int [, <strong>alpha</strong>:float])
+color = Color.hsl(<strong>hue</strong>:float, <strong>hue</strong>:float, <strong>hue</strong>:float [, <strong>alpha</strong>:float])</code></pre>
 
 <h3>Instance methods and properties</h3>
 
 <dl>
   <dt><code>channels</code></dt>
   <dd>
-    An object containing red, green, blue, and alpha properties which
-    represent the corresponding channels.
+    An array containing a color's red, green, blue,
+    and alpha data in that order.
   </dd>
   
-  <dt><code>getRGB()</code></dt>
+  <dt><code>rgb()</code></dt>
   <dd>
     Returns a CSS rgb() function representing the color.
   </dd>
   
-  <dt><code>getRGBA()</code></dt>
+  <dt><code>rgba()</code></dt>
   <dd>
-    Return a CSS rgba() function representing the color.
+    Returns a CSS rgba() function representing the color.
   </dd>
   
-  <dt><code>getValue()</code></dt>
+  <dt><code>hsl()</code></dt>
+  <dd>
+    Returns a CSS hsl() function representing the color.
+  </dd>
+  
+  <dt><code>hsla()</code></dt>
+  <dd>
+    Returns a CSS hsla() function representing the color.
+  </dd>
+  
+  <dt><code>css()</code></dt>
+  <dd>
+    If the alpha channel is 1, this returns <code>hexTriplet()</code>.
+    Otherwise, this returns <code>rgba()</code>.
+  </dd>
+  
+  <dt><code>getValue()</code> and <code>valueOf()</code></dt>
   <dd>
     Returns an integer representation of the color.
   </dd>
   
   <dt><code>setValue(<strong>value</strong>:int)</code></dt>
   <dd>
-    Set the current color based on an integer value.
+    Sets the to <code>value</code>.
   </dd>
   
-  <dt><code>toString([<strong>noHash</strong>:bool])</code> and <code>valueOf([<strong>noHash</strong>:bool])</code></dt>
+  <dt><code>hexTriplet()</code></dt>
   <dd>
-    Returns the string representation of the color excluding the alpha channel.
-    If noHash is true, the hash (#) is not included.
+    Returns the hex triplet (#RRGGBB) representation of the color.
+  </dd>
+  
+  <dt><code>rgbData()</code></dt>
+  <dd>
+    Returns an array containing the color's red, green, and
+    blue data in that order.
+  </dd>
+  
+  <dt><code>hslData()</code></dt>
+  <dd>
+    Returns an array containing the color's hue, saturation, and
+    lightness data in that order.
+  </dd>
+  
+  <dt><code>toString()</code></dt>
+  <dd>
+    Returns <code>this[Color.TOSTRING]()</code>.
   </dd>
 </dl>
 
-<h3>Class methods</h3>
+<h3>Constructor methods</h3>
 
 <dl>
   <dt><code>Color.define(<strong>colorName</strong>:string, <strong>RGB</strong>:array)</code></dt>
@@ -106,12 +149,46 @@ color = [new ]Color(<strong>red</strong>:int | string, <strong>green</strong>:in
   
   <dt><code>Color.get(<strong>colorName</strong>:string)</code></dt>
   <dd>
-    Will attempt to return any predefined colors before falling back and retrieving the computed CSS color where available (read: not IE).
+    Will attempt to return a previously defined color of the
+    name, <code>colorName</code>, before falling back and retrieving
+    the computed CSS color values where available. IE is not able to
+    retrieve computed color values.
   </dd>
   
-  <dt><code>Color.random()</code></dt>
+  <dt><code>Color.random([<strong>rangeStart</strong>:int | string] [, <strong>rangeEnd</strong>:int | string])</code></dt>
   <dd>
-    Returns a random Color instance from #000000 to #FFFFFF.
+    Returns a random color from rangeStart to rangeEnd. If rangeStart is not
+    specified, this returns a random color from #000000 to #FFFFFF. rangeStart
+    and rangeEnd can also be color names.
+  </dd>
+  
+  <dt><code>Color.rgb(<strong>red</strong>:int, <strong>green</strong>:int, <strong>blue</strong>:int [, <strong>alpha</strong>:float])</code></dt>
+  <dd>
+    Returns a color constructed from the specified RGB values.
+  </dd>
+  
+  <dt><code>Color.hsl(<strong>hue</strong>:float, <strong>hue</strong>:float, <strong>hue</strong>:float [, <strong>alpha</strong>:float])</code></dt>
+  <dd>
+    Returns a color constructed from the specified HSL values.
+  </dd>
+  
+  <dt><code>Color.RGBtoHSL(<strong>rgb</strong>:array)</code></dt>
+  <dd>
+    Returns the array of RGB values converted to HSL values.
+  </dd>
+  
+  <dt><code>Color.HSLtoRGB(<strong>hsl</strong>:array)</code></dt>
+  <dd>
+    Returns the array of HSL values converted to RGB values.
   </dd>
 </dl>
 
+<h3>Configuration properties</h3>
+<dl>
+  <dt><code>Color.TOSTRING</code></dt>
+  <dd>
+    The method name of which to call when a color instance's
+    <code>toString()</code> method is called. This defaults to
+    <code>hexTriplet</code>.
+  </dd>
+</dl>
--- color.js
+++ color.js
@@ -1,71 +1,143 @@
 /*
  * color.js
- * Version 0.1
+ * Version 0.2
  *
- * 2009-09-08
+ * 2009-09-11
  * 
  * By Elijah Grey, http://eligrey.com
  *
- * Implements a `Color' constructor.
- * 
  * License: GNU GPL v3 and the X11/MIT license
  *   See COPYING.md
  */
 
 var Color = (function () {
-	var Color = function Color (red, green, blue, alpha) {
-		var color   = this,
-		strType     = "string",
-		args        = arguments.length,
-		parseHex    = function (h) {
+	var Color = function Color (r, g, b, a) {
+		var
+		color    = this,
+		strType  = "string",
+		args     = arguments.length,
+		parseHex = function (h) {
 			return parseInt(h, 16);
 		};
 		
 		args < 4 &&
-			(alpha = 1);
+			(a = 1);
 		
 		if (args < 3) { // called as Color(color [, alpha])
-			if (typeof red === strType) {
-				red = red.substr(red.indexOf("#") + 1);
-				var threeDigits = red.length === 3;
-				red = parseHex(red);
+			if (typeof r === strType) {
+				r = r.substr(r.indexOf("#") + 1);
+				var threeDigits = r.length === 3;
+				r = parseHex(r);
 				threeDigits &&
-					(red = (((red & 0xF00) * 0x1100) | ((red & 0xF0) * 0x110) | ((red & 0xF) * 0x11)));
+					(r = (((r & 0xF00) * 0x1100) | ((r & 0xF0) * 0x110) | ((r & 0xF) * 0x11)));
 			}
 			
 			args === 2 && // alpha specifed
-				(alpha = green);
+				(a = g);
 			
-			green = (red & 0xFF00) / 0x100;
-			blue  =  red & 0xFF;
-			red   =  red >>> 0x10;
+			g = (r & 0xFF00) / 0x100;
+			b =  r & 0xFF;
+			r =  r >>> 0x10;
 		}
 		
 		if (!(color instanceof Color)) {
-			return new Color(red, green, blue, alpha);
+			return new Color(r, g, b, a);
 		}
 		
-		typeof red === strType &&
-			(red = parseHex(red));
-		typeof green === strType &&
-			(green = parseHex(green));
-		typeof blue === strType &&
-			(blue = parseHex(blue));
-		
-		this.channels = {
-			red  : red,
-			green: green,
-			blue : blue,
-			alpha: alpha
-		};
+		this.channels = [
+			typeof r === strType && parseHex(r)   || r,
+			typeof g === strType && parseHex(g)   || g,
+			typeof b === strType && parseHex(b)   || b,
+			typeof a === strType && parseFloat(a) || a
+		];
 	},
 	proto       = Color.prototype,
+	math        = Math,
 	doc         = document,
 	docEl       = doc.documentElement,
 	defaultView = doc.defaultView,
 	False       = false;
 	
-	Color.table = {};
+	Color.table = {
+		"transparent": [0, 0, 0, 0]
+	};
+	
+	// RGB to HSL and HSL to RGB code from
+	// http://www.mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
+	
+	Color.RGBtoHSL = function (rgb) {
+		// in JS 1.7 use: var [r, g, b] = rgb;
+		var r = rgb[0],
+		    g = rgb[1],
+		    b = rgb[2];
+		
+		r /= 255;
+		g /= 255;
+		b /= 255;
+		
+		var max = math.max(r, g, b),
+		    min = math.min(r, g, b),
+		h, s, l = (max + min) / 2;
+
+		if (max === min) {
+			h = s = 0; // achromatic
+		} else {
+			var d = max - min;
+			s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+			switch (max) {
+			    case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+			    case g: h = (b - r) / d + 2; break;
+			    case b: h = (r - g) / d + 4; break;
+			}
+			h /= 6;
+		}
+
+		return [h, s, l];
+
+	};
+	
+	Color.HSLtoRGB = function (hsl) {
+		// in JS 1.7 use: var [h, s, l] = hsl;
+		var h = hsl[0],
+		    s = hsl[1],
+		    l = hsl[2],
+		
+		r, g, b,
+		
+		hue2rgb = function (p, q, t){
+		    if (t < 0) t += 1;
+		    if (t > 1) t -= 1;
+		    if (t < 1/6) return p + (q - p) * 6 * t;
+		    if (t < 1/2) return q;
+		    if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+		    return p;
+		};
+		
+		if (s === 0) {
+			r = g = b = l; // achromatic
+		} else {
+			var
+			q = l < 0.5 ? l * (1 + s) : l + s - l * s,
+			p = 2 * l - q;
+			r = hue2rgb(p, q, h + 1/3);
+			g = hue2rgb(p, q, h);
+			b = hue2rgb(p, q, h - 1/3);
+		}
+
+		return [r * 0xFF, g * 0xFF, b * 0xFF];
+	};
+	
+	Color.rgb = function (r, g, b, a) {
+		return new Color(r, g, b, typeof a !== "undefined" ? a : 1)
+	};
+	
+	Color.hsl = function (h, s, l, a) {
+		var rgb = Color.HSLtoRGB([h, s, l]),
+		   ceil = math.ceil;
+		return new Color(ceil(rgb[0]), ceil(rgb[1]), ceil(rgb[2]), typeof a !== "undefined" ? a : 1);
+	};
+	
+	Color.TOSTRING = "hexTriplet"; // default toString method used
 	
 	Color.define = function (color, rgb) {
 		Color.table[color] = rgb;
@@ -80,11 +152,14 @@ var Color = (function () {
 			return False;
 		}
 		
-		var el = doc.createElement("div"),
-		style = el.style,
+		var
+		el      = doc.createElement("div"),
+		style   = el.style,
 		bgColor = "backgroundColor",
-		i,
-		rgb;
+		rgb,
+		i;
+		
+		color = color.replace(/[^a-zA-Z]/g, "").toLowerCase();
 		
 		style.display = "none";
 		
@@ -109,66 +184,99 @@ var Color = (function () {
 		
 		i = rgb.length;
 		
-		if (i < 3) {
-			return False;
-		}
-		
 		while (i--) {
-			rgb[i] = parseInt(rgb[i], 10);
+			rgb[i] = rgb[i] && parseInt(rgb[i], 10) || 0;
 		}
 		
 		return Color.apply(null, rgb);
 	};
 	
-	Color.random = function () {
+	Color.random = function (rangeStart, rangeEnd) {
+		typeof rangeStart === "string" &&
+			(rangeStart = Color.get(rangeStart)) &&
+			(rangeStart = rangeStart.getValue());
+		typeof rangeEnd === "string" &&
+			(rangeEnd = Color.get(rangeEnd)) &&
+			(rangeEnd = rangeEnd.getValue());
+		
+		var floor = math.floor,
+		   random = math.random;
+		
+		rangeEnd = (rangeEnd || 0xFFFFFF) + 1;
+		if (!isNaN(rangeStart)) {
+			return new Color(floor((random() * (rangeEnd - rangeStart)) + rangeStart));
+		}
 		// random color from #000000 to #FFFFFF
-		return new Color(Math.floor(Math.random() * 0x1000000));
+		return new Color(floor(random() * rangeEnd));
 	};
 	
-	proto.getValue = function () {
+	proto.toString = function () {
+		return this[Color.TOSTRING]();
+	};
+	
+	proto.valueOf = proto.getValue = function () {
 		var channels = this.channels;
 		return (
-			(channels.red   * 0x10000) |
-			(channels.green * 0x100  ) |
-			 channels.blue
+			(channels[0] * 0x10000) |
+			(channels[1] * 0x100  ) |
+			 channels[2]
 		);
 	};
 	
 	proto.setValue = function (value) {
-		this.channels = {
-			red  :  value >>> 0x10,
-			green: (value & 0xFF00) / 0x100,
-			blue :  value & 0xFF,
-			alpha:  this.channels.alpha
-		};
+		this.channels.splice(
+			0, 3,
+			
+			value >>> 0x10,
+			(value & 0xFF00) / 0x100,
+			value & 0xFF
+		);
 	};
 	
-	proto.getRGB = function () {
-		var channels = this.channels;
-		return "rgb(" + channels.red + "," + channels.green + "," + channels.blue + ")";
-	};
-	
-	proto.getRGBA = function () {
-		var channels = this.channels;
-		return "rgba(" + channels.red + "," + channels.green + "," + channels.blue + "," + channels.alpha + ")";
-	};
-	
-	proto.valueOf = proto.toString = ("01".substr(-1) === "1" ?
-		function (noHash) {
-			// pad value 6 zeros to the left
-			return (noHash ? "" : "#") + ("00000" + this.getValue().toString(16)).substr(-6);
+	proto.hexTriplet = ("01".substr(-1) === "1" ?
+	// pad 6 zeros to the left
+		function () {
+			return "#" + ("00000" + this.getValue().toString(16)).substr(-6);
 		}
 	: // IE doesn't support substr with negative numbers
-		function (noHash) {
-			// pad value 6 zeros to the left
-			// return (noHash ? "" : "#") + ("00000" + this.getValue().toString(16)).substr(-6);
-			var str = this.getValue().toString(16),
-				width = 6,
-				f = (new Array( str.length < Math.abs(width) ? Math.abs(width) - str.length + 1 : 0)).join("0");
-		
-			return (noHash ? "" : "#") + (width > 0 ? f + str : str + f);
+		function () {
+			var str = this.getValue().toString(16);
+			return "#" + (new Array( str.length < 6 ? 6 - str.length + 1 : 0)).join("0") + str;
 		}
 	);
+	
+	proto.css = function () {
+		var color = this;
+		return color.channels[3] === 1 ? color.hexTriplet() : color.rgba();
+	};
+	
+	proto.rgbData = function () {
+		return this.channels.slice(0, 3);
+	};
+	
+	proto.hslData = function () {
+		return Color.RGBtoHSL(this.rgbData());
+	};
+	
+	// TODO: get rid of redundancy in the following methods
+	
+	proto.rgb = function () {
+		return "rgb(" + this.rgbData().join(",") + ")";
+	};
+	
+	proto.rgba = function () {
+		return "rgba(" + this.channels.slice(0, 4).join(",") + ")";
+	};
+	
+	proto.hsl = function () {
+		var hsl = this.hslData();
+		return "hsl(" + hsl[0] * 360 + "," + (hsl[1] * 100) + "%," + (hsl[2] * 100) + "%)";
+	};
+	
+	proto.hsla = function () {
+		var hsl = this.hslData();
+		return "hsla(" + hsl[0] * 360 + "," + (hsl[1] * 100) + "%," + (hsl[2] * 100) + "%," + this.channels[3] + ")";
+	};
 	
 	return Color;
 }());
